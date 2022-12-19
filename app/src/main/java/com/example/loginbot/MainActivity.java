b2c35ca3400fb1988dab7f43ce2489a3e9e47a85
sanketshirsath226@gmail.com <sanketshirsath226@gmail.com>
2022-12-19 22:44:22 +0530

    Initial Created Service and Add Features to Detect App.

--- app/src/main/java/com/example/loginbot/MainActivity.java
+++ app/src/main/java/com/example/loginbot/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,12 +21,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Context ctx=this;
+        checkOverlayPermission();
+        startService();
         try {
             Intent i = ctx.getPackageManager().getLaunchIntentForPackage("com.pubg.imobile");
             ctx.startActivity(i);
         } catch (Exception e) {
             Log.d("Log",e.getMessage());
         }
+    }
+
+    // method for starting the service
+    public void startService(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // check if the user has already granted
+            // the Draw over other apps permission
+            if(Settings.canDrawOverlays(this)) {
+                // start the service based on the android version
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(new Intent(this, ForegroundService.class));
+                } else {
+                    startService(new Intent(this, ForegroundService.class));
+                }
+            }
+        }else{
+            startService(new Intent(this, ForegroundService.class));
+        }
+    }
+
+    // method to ask user to grant the Overlay permission
+    public void checkOverlayPermission(){
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                // send user to the device settings
+                Intent myIntent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+                startActivity(myIntent);
+            }
+        }
+    }
+
+    // check for permission again when user grants it from
+    // the device settings, and start the service
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startService();
     }
 
 }
